88f3a64ff3b9ef1b90d1bc5a4acd202a96acb172
Radioactive Potato / krunchiekrunch <75719839+RadioactivePotato@users.noreply.github.com>
2024-06-07 18:29:07 +0100

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         GeoFS Camera Reset
+// @version      1.0
+// @description  Reset GeoFS camera when Shift+C is pressed
+// @author       krunchiekrunch
+// @match        https://www.geo-fs.com/geofs.php?v=*
+// @match        https://*.geo-fs.com/geofs.php*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // detect when middle mouse button is pressed
+    document.addEventListener('mousedown', function(event) {
+        if (event.button === 1) {
+            // reset camera
+            if (typeof geofs !== 'undefined' && geofs.camera && typeof geofs.camera.reset === 'function') {
+                geofs.camera.reset();
+            }
+        }
+    }, false);
+})();
